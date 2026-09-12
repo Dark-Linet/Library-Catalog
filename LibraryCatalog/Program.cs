cd84e4f9a94ef0c1e4f1dcd80b01e8a901f19a18
Dark-Linet <Maks.ver14@yandex.ru>
2026-09-12 23:48:53 +0300

    Добавить выбор хранилища параметром запуска

--- LibraryCatalog/Program.cs
+++ LibraryCatalog/Program.cs
@@ -1,38 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using LibraryCatalog.Logic;
 using LibraryCatalog.Data;
-using System.IO;
 
-namespace LibraryCatalog
+namespace LibraryCatalog;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string jsonPath = Path.Combine(AppContext.BaseDirectory, "books.json");
+        string xmlPath = Path.Combine(AppContext.BaseDirectory, "books.xml");
+       
+        string kind = args.Length > 0 ? args[0] : "json";
+        
+        IBookRepository repository = kind switch
         {
-            string jsonPath = Path.Combine(AppContext.BaseDirectory, "books.json");
+            "xml" => new XmlBookRepository(xmlPath),
+            "memory" => new BookRepository(),
+            "demo" => new DemoBookRepository(),
+            _ => new JsonBookRepository(jsonPath)
+        };
 
-            IBookRepository repository = new JsonBookRepository(jsonPath);
+        Console.WriteLine($"Выбранный тип хранилища: {kind}\n");
 
-            var service = new BookService(repository);
+        var service = new BookService(repository);
 
-            Console.WriteLine("Введите название новой книги: ");
-            string title = Console.ReadLine() ?? "";
+        Console.WriteLine("Введите название новой книги: ");
+        string title = Console.ReadLine() ?? "";
 
-            Console.WriteLine("Введите год издания: ");
-            int.TryParse(Console.ReadLine(), out int year);
+        Console.WriteLine("Введите год издания: ");
+        int.TryParse(Console.ReadLine(), out int year);
 
-            service.AddBook(title, year);
+        service.AddBook(title, year);
 
-            Console.WriteLine("\nОтобранные записи:");
-            
-            foreach (var item in service.GetImportant())
-            {
-                Console.WriteLine($"{item.Id}: {item.Title}");
-            }
+        Console.WriteLine("\nОтобранные записи (до 2000 года):");
+        foreach (var item in service.GetImportant())
+        {
+            Console.WriteLine($"{item.Id}: {item.Title} ({item.Year})");
+        }
+       
+        Console.WriteLine("\nСамая старая книга в каталоге:");
+        Book? oldestBook = service.GetOldestBook();
+        if (oldestBook != null)
+        {
+            Console.WriteLine($"\"{oldestBook.Title}\", изданная в {oldestBook.Year} году.");
+        }
+        else
+        {
+            Console.WriteLine("Каталог пуст.");
         }
     }
 }
