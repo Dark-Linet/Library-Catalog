60ac5ae961451d94058015aa95d116096cb38b2f
Dark-Linet <Maks.ver14@yandex.ru>
2026-09-12 23:46:16 +0300

    Расширить контракт хранилища методом добавления

--- LibraryCatalog.Logic/BookService.cs
+++ LibraryCatalog.Logic/BookService.cs
@@ -1,38 +1,35 @@
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.Linq;
 
 namespace LibraryCatalog.Logic;
 
 public class BookService
 {
     private readonly IBookRepository _repository;
-
-    public BookService(IBookRepository repository)
-    {
-        _repository = repository;
-    }
-
-    public List<Book> GetImportant()
-    {
-        return _repository.GetAll()
-            .Where(item => item.Year < 2000)
-            .ToList();
-    }
+    
+    public BookService(IBookRepository repository) => _repository = repository;
+    
+    public List<Book> GetImportant() => _repository.GetAll()
+        .Where(item => item.Year < 2000)
+        .ToList();
 
     public void AddBook(string title, int year)
-    {      
-        if (string.IsNullOrWhiteSpace(title))
-        {
-            return;
-        }
-        
+    {
+        if (string.IsNullOrWhiteSpace(title)) return;
+
         int nextId = _repository.GetAll().Count + 1;
-       
+
         _repository.Add(new Book
         {
             Id = nextId,
             Title = title,
             Year = year
         });
+    }
+   
+    public Book? GetOldestBook()
+    {
+        var books = _repository.GetAll();
+        return books.Count == 0 ? null : books.OrderBy(b => b.Year).First();
     }
 }
