8f138b419cdfecbc2218a19c47c76095d5000950
Dark-Linet <Maks.ver14@yandex.ru>
2026-09-12 23:47:58 +0300

    Добавить хранилища в форматах JSON и XML

--- LibraryCatalog.Data/BookRepository.cs
+++ LibraryCatalog.Data/BookRepository.cs
@@ -6,19 +6,13 @@ public class BookRepository : IBookRepository
 {
     private readonly List<Book> _items = new()
     {
-        new Book { Id = 1, Title = "Мастер и Маргарита", Year = 1967},
-        new Book { Id = 2, Title = "Властелин колец", Year = 1954},
-        new Book { Id = 3, Title = "Голодные игры", Year = 2008},
-        new Book { Id = 4, Title = "Дюна: Батлерианский джихад", Year = 2002},
+        new() { Id = 1, Title = "Мастер и Маргарита", Year = 1967 },
+        new() { Id = 2, Title = "Властелин колец", Year = 1954 },
+        new() { Id = 3, Title = "Голодные игры", Year = 2008 },
+        new() { Id = 4, Title = "Дюна: Батлерианский джихад", Year = 2002 }
     };
 
-    public List<Book> GetAll()
-    {
-        return _items;
-    }
+    public List<Book> GetAll() => _items;
 
-    public void Add (Book item)
-    {
-        _items.Add(item);
-    }
+    public void Add(Book item) => _items.Add(item);
 }
--- LibraryCatalog.Data/DemoBookRepository.cs
+++ LibraryCatalog.Data/DemoBookRepository.cs
@@ -1,19 +1,15 @@
-using System.Collections.Generic;
 using LibraryCatalog.Logic;
 
 namespace LibraryCatalog.Data;
 
 public class DemoBookRepository : IBookRepository
 {
-    public List<Book> GetAll()
-        {
-        return new List<Book>
-        {
-            new Book { Id = 100, Title = "Демонстрационная книга", Year = 1990}
-        };
-    }
+    public List<Book> GetAll() => new()
+    {
+        new() { Id = 100, Title = "Демонстрационная книга", Year = 1990 }
+    };
 
-    public void Add (Book item)
+    public void Add(Book item)
     {
         // Демонстрационное хранилище доступно только для чтения
     }
